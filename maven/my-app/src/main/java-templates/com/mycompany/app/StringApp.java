6cf6abea948bca5ec4ecce01bfaf036e2c259ee7
James Hook <hempnall@yahoo.co.uk>
2015-10-19 20:10:30 +0100

    added maven example

---
+++ maven/my-app/src/main/java-templates/com/mycompany/app/StringApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringApp 
+{
+    public static String getString(  )
+    {
+        return "${my_os_var}";
+    }
+}
---
+++ maven/my-app/src/test/java/com/mycompany/app/newTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class newTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+
+
+    @Test
+    public void AppTest(  )
+    {
+        Assert.assertEquals(5+4, 3+6);
+    }
+
+
+}
