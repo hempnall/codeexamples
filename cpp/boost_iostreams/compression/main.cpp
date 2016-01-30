59b814182a3f4ece6ba55052b1ba60032f41eeb6
James Hook <hempnall@yahoo.co.uk>
2016-01-30 16:29:15 +0000

    added pdf example to compression

--- cpp/boost_iostreams/compression/main.cpp
+++ cpp/boost_iostreams/compression/main.cpp
@@ -26,4 +26,40 @@ int main()
     in.push(ifile);
     boost::iostreams::copy(in, std::cout);
     }
+
+    {
+    std::ofstream ofile("hello.z", std::ios_base::out | std::ios_base::binary);
+    bo::filtering_ostream out;
+    out.push(bo::zlib_compressor());
+    out.push(ofile);
+    out << "This is a gz file\n";
+    }
+
+    {
+        std::ifstream ifile("hello.z", std::ios_base::in | std::ios_base::binary);
+        bo::filtering_streambuf<bo::input> in;
+        in.push(bo::zlib_decompressor());
+        in.push(ifile);
+        boost::iostreams::copy(in, std::cout);
+    }
+
+    {
+        // this one will error on the compreesion found in FlateDecode pdf streams!!! - use zlib instead
+//        std::ifstream ifile("../out3.bin", std::ios_base::in | std::ios_base::binary);
+//        bo::filtering_streambuf<bo::input> in;
+//        in.push(bo::gzip_decompressor());
+//        in.push(ifile);
+//        boost::iostreams::copy(in, std::cout);
+    }
+
+
+    {
+        // this is the one you need for compression in pdf files
+        std::ifstream ifile("../out3.bin", std::ios_base::in | std::ios_base::binary);
+        bo::filtering_streambuf<bo::input> in;
+        in.push(bo::zlib_decompressor());
+        in.push(ifile);
+        boost::iostreams::copy(in, std::cout);
+    }
+
 }
